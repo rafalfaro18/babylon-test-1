a32c5d6212d48fd5bb138ed45aa760e5d8057d71
Rafael Alfaro <rafalfaro_18@outlook.com>
2023-06-18 15:49:22 -0600

    Adds virtual joystick controls for mobile

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,7 @@ import { GridMaterial } from '@babylonjs/materials/grid/gridMaterial';
 import "@babylonjs/core/Materials/standardMaterial";
 
 import * as earcut from "earcut"
+import { VirtualJoystick } from '@babylonjs/core/Misc/virtualJoystick';
 
 //import "@babylonjs/core/Debug/debugLayer"; // Augments the scene with the debug methods
 //import "@babylonjs/inspector"; // Injects a local ES6 version of the inspector to prevent automatically relying on the none compatible version
@@ -76,6 +77,50 @@ var ground = CreateGround('ground1', { width: 6, height: 6, subdivisions: 2 }, s
 
 // Affect a material
 ground.material = material;
+
+// Create joystick and set z index to be below playgrounds top bar
+var leftJoystick = new VirtualJoystick(true);
+var rightJoystick = new VirtualJoystick(false);
+VirtualJoystick.Canvas.style.zIndex = "4";
+
+var movespeed = 5
+scene.onBeforeRenderObservable.add(()=>{
+    if(leftJoystick.pressed){
+        var moveX = leftJoystick.deltaPosition.x * (engine.getDeltaTime()/1000) * movespeed;
+        var moveZ = leftJoystick.deltaPosition.y * (engine.getDeltaTime()/1000) * movespeed;
+        camera.position.x+=moveX
+        camera.position.z+=moveZ
+        
+    }
+    if(rightJoystick.pressed){
+        var moveY = rightJoystick.deltaPosition.y * (engine.getDeltaTime()/1000) * movespeed;
+        camera.position.y+=moveY
+    }
+})
+
+// Create button to toggle joystick overlay canvas
+var btn = document.createElement("button")
+btn.innerText = "Enable/Disable Joystick"
+btn.style.zIndex = "10";
+btn.style.position = "absolute"
+btn.style.bottom = "50px"
+btn.style.right = "0px"
+document.body.appendChild(btn)
+
+// Button toggle logic
+btn.onclick = ()=>{
+    if(VirtualJoystick.Canvas.style.zIndex == "-1"){
+        VirtualJoystick.Canvas.style.zIndex = "4";
+    }else{
+        VirtualJoystick.Canvas.style.zIndex = "-1";
+    }
+}
+
+// Dispose button on rerun
+scene.onDisposeObservable.add(()=>{
+    VirtualJoystick.Canvas.style.zIndex = "-1";
+    document.body.removeChild(btn)
+})
 
 // Render every frame
 engine.runRenderLoop(() => {
