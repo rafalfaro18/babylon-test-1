a8d0730ba13f78997c37ad17c7adc98fb7d26766
Rafael Alfaro <rafalfaro_18@outlook.com>
2023-06-17 23:50:50 -0600

    Adds commented inspector support

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,9 @@ import { Scene } from '@babylonjs/core/scene';
 
 import { GridMaterial } from '@babylonjs/materials/grid/gridMaterial';
 
+//import "@babylonjs/core/Debug/debugLayer"; // Augments the scene with the debug methods
+//import "@babylonjs/inspector"; // Injects a local ES6 version of the inspector to prevent automatically relying on the none compatible version
+
 // Get the canvas element from the DOM.
 const canvas = document.getElementById("renderCanvas") as HTMLCanvasElement;
 
@@ -17,6 +20,8 @@ const engine = new Engine(canvas);
 
 // Create our first scene.
 var scene = new Scene(engine);
+
+//scene.debugLayer.show();
 
 // This creates and positions a free camera (non-mesh)
 var camera = new FreeCamera("camera1", new Vector3(0, 5, -10), scene);
